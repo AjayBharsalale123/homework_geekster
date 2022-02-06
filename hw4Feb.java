c9a745e69dc07bc45d1b5085c904f3f4e38d9d69
AjayBharsalale123 <60804925+AjayBharsalale123@users.noreply.github.com>
2022-02-06 13:54:43 +0530

    Add files via upload
    homework of 4 feb 2022

---
+++ hw4Feb.java
@@ -0,0 +1,176 @@
+package hw_Geekster;
+import java.util.Scanner;
+
+public class hw4Feb {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter a number : ");
+//		int n=sc.nextInt();
+//		
+//		System.out.println(reverseNumber(n));
+//		
+//		 int num,  sum = 0;
+//
+//	        
+//	        System.out.print("Enter any number : ");
+//	        num = sc.nextInt();
+//
+//
+//	        while(num != 0) {
+//
+//	            sum = sum+num % 10;
+//
+//	           
+//	            num = num/10;
+//	        }
+//
+//	        System.out.println("\nSum of all digits in "  + sum);
+//		
+		
+	        
+		
+//		System.out.println("Enter a number :\n");
+//		int n=sc.nextInt();
+//		
+//		int first=n/10000;
+//		n=n%10000;
+//		
+//		int second=n/1000;
+//		n=n%1000;
+//		
+//		int thid=n/100;
+//		n=n%100;
+//		
+//		int fourth=n/10;
+//		int fifth=n%10;
+//		
+//		int sum=first + fourth;
+//		System.out.println("Sum ="+sum);
+//		
+			
+		
+		
+		
+		
+		
+//		int total=90;
+//		
+//		int gradeStudentsA= total*50/100;
+//		System.out.println("The Students who got A grade total is :"+gradeStudentsA+"%");
+//		
+//		int gradeAboys=20;
+//		
+//		int gradeGirls=gradeStudentsA-gradeAboys;
+//		System.out.println("The Number of A Grade Girls is :"+gradeGirls+"%");
+		
+		
+//		System.out.println("Enter Fahrenheit :");
+//		float f=sc.nextFloat();
+//		System.out.println(fToC(f));
+		
+//		int a=5,b=8;
+//		System.out.println("Before Swapping : a=5  b=8");
+//		System.out.println("After Swapping :");
+//		
+//		withThirdVariable(a,b);
+//		withoutThirdVariable(a,b);
+		
+//		
+//		int s1=78,s2=45,s3=62;
+//		System.out.println(robertTotalmarks(s1,s2,s3));
+//		System.out.println(robertPercentagemarks(s1,s2,s3)+"%");
+//		
+//		
+		//int a=55,b=70;
+        //System.out.println(a<50 || a<b);
+		
+        //int a=55,b=70;
+        //System.out.println(a<50 && a<b);
+		
+		//System.out.println(Math.pow(7, 5));
+		
+		//System.out.println(23==45);
+		
+		//System.out.println((((8+2345)/3)%5)*5);
+		
+		//int a=2,b=3,c=5;
+		//System.out.println(periTriangle(a,b,c));
+		
+	    //int n=5,m=7;
+		//System.out.println(areaRectangle(n,m));
+		//System.out.println(periRectangle(n,m));
+
+	}
+	
+//	public static int reverseNumber(int a) {
+//		
+//		int first=a/100;
+//		a=a%100;
+//		
+//		int second=a/10;
+//		int third=a%10;
+//		
+//		int rev=third*100+second*10+first;
+//		
+//		return rev;
+//	}
+	
+	
+	
+//	public static float fToC(float a) {
+//		float Fn=(a-32)*5/9;
+//		return Fn;
+//	}
+	
+//	public static void withThirdVariable(int a,int b) {
+//		int c=a;
+//		a=b;
+//		b=c;
+//		
+//		
+//		System.out.println("\nSwapping using Third Variable");
+//		System.out.println("a = "+a+"b = "+b);
+//		
+//	}
+//	
+//	public static void withoutThirdVariable(int a,int b) {
+//		a=a+b;
+//		b=a-b;
+//		a=a-b;
+//		
+//		System.out.println("\nSwapping without using Third Variable");
+//		System.out.println("a = "+a+"b = "+b);
+//		
+//	}
+//	public static int robertTotalmarks(int a,int b,int c) {
+//		int d=a+b+c;
+//		return d;
+//	}
+//	
+//	public static int robertPercentagemarks(int a,int b,int c) {
+//		int d=(a+b+c)*100/300;
+//		return d;
+//	}
+	
+//	public static int periTriangle(int a,int b, int c) {
+//		int d=a+b+c;
+//		return d;
+//	}
+	
+//	public static int areaRectangle(int a,int b) {
+//		int area=a*b;
+//		return area;
+//		
+//	}
+//	
+//	public static int periRectangle(int a,int b) {
+//		int area=2*(a+b);
+//		return area;
+//		
+//	}
+	
+	
+}
